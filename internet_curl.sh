b8881bbd745b1fb39567d465104dbfdfb79f0f6b
Jan Lelis <mail@janlelis.de>
2010-06-03 12:37:14 +0200

    curl

--- internet_curl.sh
+++ internet_curl.sh
@@ -1,11 +1,23 @@
 #!/bin/sh
 # [Gedit Tool]
 # Name=[internet] curl
-# Shortcut=<Control><Alt>u
 # Applicability=all
-# Output=new-document
+# Output=nothing
 # Input=nothing
 # Save-files=nothing
 
 
-curl `zenity --entry`
+# Creates a new document and downloads the entered URL (if valid)
+#  (depends on curl, zenity)
+#
+# Save:   Nothing
+# Input:  Nothing
+# Output: Nothing
+#
+# by Jan Lelis (mail@janlelis.de), edited by (you?)
+
+params=`zenity --entry --title='curl' --text='params:' --width=500`
+
+if [ ! -z "$params" ]; then
+  curl "$params" | zenity --text-info --title="curl $params" --width=1000 --height=700
+fi
