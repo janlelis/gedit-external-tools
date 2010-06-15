471a5cea598380dca0b74d280532b0f97f4b89fd
Jan Lelis <mail@janlelis.de>
2010-06-15 23:02:23 +0200

    [git] changed diff to difftool

--- git_diff.sh
+++ git_diff.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 # [Gedit Tool]
 # Name=[git] diff
-# Shortcut=<Control><Alt>f
+# Shortcut=<Control><Alt>i
 # Applicability=titled
 # Output=nothing
 # Input=nothing
 # Save-files=document
 
 
-# Saves the current file and shows it differences from current HEAD
-#  (depends on git, zenity)
+# Saves the current file and shows it differences from current HEAD in meld
+#  (depends on git, meld, zenity)
 #
 # Save:   Current document
 # Input:  Nothing
@@ -18,12 +18,11 @@
 # by Jan Lelis (mail@janlelis.de), edited by (you?)
 
 if [ ! -z `git rev-parse --git-dir` ]; then
-  diff=`git diff $GEDIT_CURRENT_DOCUMENT_PATH` &&
+  diff=`git difftool -t meld $GEDIT_CURRENT_DOCUMENT_PATH` &&
   if [ -z "$diff" ]; then
     zenity --info --title="git diff" --text='File has not changed.'
-  else
-    echo "$diff" | zenity --text-info --title="git diff $GEDIT_CURRENT_DOCUMENT_PATH" --width=1000 --height=700 
   fi
 else
   zenity --error --title='git diff' --text='Sorry, not a git repository'
 fi
+
